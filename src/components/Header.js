f439762825dc43ce1a2142dc00d11a6a332770cb
larissagoularte <58703854+larissagoularte@users.noreply.github.com>
2024-05-23 22:03:38 +0100

    init

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header class="text-white bg-red-900 body-font">
+      <div class="container mx-auto flex flex-wrap justify-center md:p-3 pt-2 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center mb-4 md:mb-0">
+          <span class="ml-3 text-xl">MJManagement</span>
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
